6fa788218b25667a50591e762e91c11924452b62
Ricky Arora <your_email_here>
2020-07-09 16:37:35 -0400

    cat model + migration done

---
+++ app/models/cat.rb
@@ -0,0 +1,18 @@
+require 'action_view'
+require 'action_view/helpers'
+include ActionView::Helpers::DateHelper
+class Cat < ApplicationRecord
+    SEX = ['M','F']
+    COLORS_LIST = ['blue','yellow','red']
+    validates :birth_date, presence: true 
+    validates :color, inclusion: { in:  COLORS_LIST }, presence: true
+    validates :name, :description, presence: true
+    validates :sex, inclusion: {in: SEX} ,presence: true
+    
+    def age 
+        current_time = Date.today
+        birth_time = Date.parse(:birth_date)
+        current_age = distance_of_time_in_words(birth_time, current_time)
+        current_age
+    end
+end
