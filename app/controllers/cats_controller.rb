fa94cc484b92f4cb868697f387b4c8046b3bd45d
Ricky Arora <your_email_here>
2020-07-16 17:49:44 -0400

    CRUD

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,8 +5,47 @@ class CatsController < ApplicationController
     end
 
     def show 
-        cat = Cat.find_by(id: params[:id])
-        render json: cat
+        @cat = Cat.find_by(id: params[:id])
+        if !@cat.nil?
+            render :show
+        else
+            redirect_to '/cats'
+        end
+    end
+
+    def new
+        render :new
+    end
+
+    def create 
+        @cat = Cat.new(cat_params)
+        if @cat.save!
+            #show user the cat index
+            redirect_to cat_url(@cat)
+        else 
+            #show success page
+            render :new
+        end
+    end
+
+    def edit 
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+
+        if @cat.update_attributes(cat_params)
+            redirect_to cats_url
+        else 
+            render :edit
+        end
+    end
+
+    private 
+    def cat_params 
+        params.require(:cat).permit(:birth_date, :color, :name, :description, :sex)
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(birth_date:'1996/05/11', color: 'red', name: 'Ricky', sex: 'M', description: 'CUTE CAT')
+Cat.create(birth_date:'1995/06/13', color: 'blue', name: 'Sharan', sex: 'F', description: 'CUTE CAT2')
+Cat.create(birth_date:'1994/07/12', color: 'red', name: 'Arora', sex: 'M', description: 'CUTE CAT3')
+Cat.create(birth_date:'1993/08/19', color: 'blue', name: 'Lubana', sex: 'F', description: 'CUTE CAT4')
+Cat.create(birth_date:'1992/09/14', color: 'red', name: 'Baby', sex: 'M', description: 'CUTE CAT5')
