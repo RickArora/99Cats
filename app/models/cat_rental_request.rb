4ac9fa9b08806320f652faff567c43a26270c01d
Ricky Arora <your_email_here>
2020-07-22 18:06:47 -0400

    n

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -9,4 +9,15 @@ class CatRentalRequest < ApplicationRecord
         class_name: :cat
     ], dependent: :destroy)
 
+    def overlapping_requests 
+        CatRentalRequest.where.not('end_date < #{self.start_date} OR start_date > #{self.end_date}').where('where cat_id = #{self.cat_id}')
+    end
+
+    def overlapping_approved_requests
+        overlapping_requests.where('id = #{self.id}')
+    end
+
+    def does_not_overlap_approved_request
+        !overlapping_approved_requests.exists
+    end
 end
